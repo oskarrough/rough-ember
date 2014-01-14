25e2e163d5dcdb50d6c66affec7f76cf33c3d63f
Oskar Rough <oskarrough@gmail.com>
2014-01-14 23:24:03 +0100

    Add boiler Ember app with posts and pages

---
+++ app/scripts/main.js
@@ -0,0 +1,43 @@
+App = Ember.Application.create({});
+
+App.Router.map(function() {
+  this.resource('about');
+  this.resource('posts', function() {
+    this.resource('post', { path: ':post_id' });
+  });
+});
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return posts;
+  }
+});
+
+App.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return posts.findBy('id', params.post_id);
+  }
+});
+
+App.PostController = Ember.ObjectController.extend({
+  // isEditing: false,
+
+  // edit: function() {
+  //   this.set('isEditing', true);
+  // },
+
+  // doneEditing: function() {
+  //   this.set('isEditing', false);
+  //   this.get('store').commit();
+  // }
+});
+
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.helper('format-markdown', function(input) {
+  return new Handlebars.SafeString(showdown.makeHtml(input));
+});
+
+Ember.Handlebars.helper('format-date', function(date) {
+  return moment(date).fromNow();
+});
