64e15943a96a47f798861f1998b905c0bb552ddf
Oskar Rough <oskarrough@gmail.com>
2014-01-31 21:24:13 +0100

    Split data per content type

--- app/data/page.js
+++ app/data/page.js
@@ -10,17 +10,3 @@ App.Page.FIXTURES = [
 		body: 'Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text\n\nLots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of\n\ntext Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text Lots of text.'
 	}
 ];
-
-App.Post.FIXTURES = [
-	{
-		id: 1,
-		title: "One post",
-		body: "Yes this is my first post."
-	},
-	{
-		id: 2,
-		title: "Another post",
-		body: "This is written in markdown.\n\n##Hello\n\nhello",
-		isFeatured: true
-	}
-];
